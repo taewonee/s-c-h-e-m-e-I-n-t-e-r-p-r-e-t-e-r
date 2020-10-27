2bf227a132fc746b4da1f55729e2946c24336669
taewonee <twoyearsone@gmail.com>
2020-10-28 03:18:42 +0900

    read function completed

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 // hash table funciton 먼저 만들 것 10월 27일의 목표
 // 즉, symbol로 먼저 나누어야함 ㅇㅇ
-// symbol을 나누는 기준: ' ' 가 들어오면 ' ' 이전의 모든 녀석들이 같은 symbol 
-// '('가 들어오면 ')'가 들어오기 전까지?? 
+// symbol을 나누는 기준: ' ' 가 들어오면 ' ' 이전의 모든 녀석들이 같은 symbol
+// '('가 들어오면 ')'가 들어오기 전까지??
 // ex. (define (square x) (* x x))
 // 괄호는 아직 잘 모르겠고,,, 음 일단 ' ' 가 있으면 그 전까지는 symbol이 맞는듯
 // 그럼일단 main에서 한줄로 받아서, hash하고 바로 조져야하나?
@@ -17,11 +17,13 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
-
+unsigned int StringToInt(string s);
 class Hash_Table
 {
 public:
     int hashFind(string hashKey);
+    void print();
+
 private:
     int capacity = 10;
     class hash_cell
@@ -30,7 +32,7 @@ private:
         string Symbol;
         hash_cell *link;
     };
-    hash_cell * hash_table = new hash_cell[capacity];
+    hash_cell *hash_table = new hash_cell[capacity];
     //    hash_cell* hash_table = new hash_cell[10];
 };
 
@@ -38,47 +40,40 @@ class Node_Array
 {
 public:
     void nodearrayprocess(string input);
+    int Read(string *token, Hash_Table *HT);
+    void print();
 
 private:
     class node_cell
     {
+        friend Node_Array;
         int left;
         int right;
     };
-    node_cell node_array[30];
-    int current = 1;
+    node_cell node_array[300];
+    int current = 0;
     //constructor로 옮겨야한다
 };
-
-int main()
-{
-    Hash_Table HT;
-    Node_Array NA;
-    string input;
-    cin >> input;
-    //NA.NodeArrayProcess(input);
-
-    return 0;
-}
 
 // get a hash value;
 int Hash_Table::hashFind(string hashKey)
 {
     int hashValue = StringToInt(hashKey) % capacity;
+    //   cout << "STI: " << StringToInt(hashKey);
+    //   cout << "HV is: " << hashValue;
     int temp = hashValue;
-    for(; hashValue<temp+capacity;hashValue++)
+    for (int i = 0; i < capacity; i++)
     {
-        if (hash_table[hashValue].Symbol.compare(hashKey) || hash_table[hashValue].Symbol.empty())
+        if (hashKey.compare(hash_table[hashValue].Symbol) == 0 || hash_table[hashValue].Symbol.empty())
         {
             hash_table[hashValue].Symbol = hashKey;
             return -hashValue;
         }
+        hashValue = (hashValue + 1) % capacity;
     }
-    cout<<"Somethig is error";
     throw("hash is full for now");
-
 };
-
+/*
 void Node_Array::nodearrayprocess(string input)
 {
     char k;
@@ -102,20 +97,136 @@ void Node_Array::nodearrayprocess(string input)
         node_array[current].right = ++current;
     }
 };
+*/
 //constructor 필요
 //
 
-unsigned int StringToInt(string s){
+unsigned int StringToInt(string s)
+{
     int length = (int)s.length();
     unsigned int answer = 0;
-    if(length%2==1)
+    if (length % 2 == 1)
     {
-        answer=s.at(length-1);
+        answer = s.at(length - 1);
         length--;
     }
-    for(int i =0; i<length;i+=2){
-        answer+=s.at(i);
-        answer+=((int)s.at(i+1))<<8;
+    for (int i = 0; i < length; i += 2)
+    {
+        answer += s.at(i);
+        answer += ((int)s.at(i + 1)) << 8;
     }
     return answer;
 };
+
+string next_token(string *sentence)
+{
+    if (sentence->empty())
+        return "";
+    int k = sentence->find(" ");
+    if (k == 0) //remove useless space
+        return next_token(&(*sentence = sentence->substr(1, -1)));
+    string symbol = sentence->substr(0, k);
+    if (symbol.at(0) == '(') //check the first char is '(' thinking about "(variable" or "( variable"
+    {
+        symbol = "(";
+        *sentence = sentence->substr(1, -1);
+        return symbol;
+    }
+    if (symbol.at(0) == ')')
+    {
+        symbol = ")";
+        *sentence = sentence->substr(1, -1);
+        return symbol;
+    }
+    if (symbol.back() == ')')
+    {
+        while (symbol.back() == ')')
+        {
+            symbol.pop_back();
+        }
+        *sentence = sentence->substr(symbol.length(), -1);
+        return symbol;
+    }
+    if (k != -1)
+        *sentence = sentence->substr(k, -1);
+    if (k == -1)
+        *sentence = "";
+    return symbol;
+}
+
+int main()
+{
+    Hash_Table HT;
+    Node_Array NA;
+    string input;
+    getline(cin, input);
+    //    input = "( define (square x (* x x)))";
+    NA.Read(&input, &HT);
+    HT.print();
+    cout << endl;
+    NA.print();
+    return 0;
+}
+
+//흠 일단 current는 보존
+
+int Node_Array::Read(string *input, Hash_Table *HT)
+{
+    int root = 0;
+    bool first = true;
+    int token_hash_value = HT->hashFind(next_token(input));
+    if (token_hash_value == HT->hashFind("("))
+    {
+        int temp;
+        token_hash_value = HT->hashFind(next_token(input));
+        while (token_hash_value != HT->hashFind(")"))
+        {
+            if (first)
+            {
+                //root = temp = alloc();
+                current++;
+                root = temp = current;
+                first = false;
+            }
+            else
+            {
+                //node_array[temp].right = alloc();
+                current++;
+                node_array[temp].right = current;
+                temp = node_array[temp].right;
+            }
+            if (token_hash_value == HT->hashFind("("))
+            {
+                *input = "(" + *input;
+                node_array[temp].left = Read(input, HT);
+            }
+            else
+            {
+                node_array[temp].left = token_hash_value;
+            }
+            if (!first)
+            {
+                node_array[temp].right = -1;
+            }
+            token_hash_value = HT->hashFind(next_token(input));
+        }
+        return root;
+    }
+    else
+    {
+        return token_hash_value;
+    }
+}
+
+void Node_Array::print()
+{
+    for (int i = 1; i < current + 1; i++)
+        cout << "current: " << i << " l:" << node_array[i].left << " r:" << node_array[i].right << endl;
+}
+
+void Hash_Table::print()
+{
+    for (int i = 0; i < capacity; i++)
+        if (!hash_table[i].Symbol.empty())
+            cout << "index: " << i << " symbol: " << hash_table[i].Symbol << endl;
+}
